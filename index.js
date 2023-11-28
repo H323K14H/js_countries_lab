53b7179860f594bf752de20eda166950115a4ba4
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-28 19:18:45 +0000

    Introduced abstract functions for enhanced code reusability and modularity:
    - Abstracted country data fetching logic into a dedicated fetchCountryData function.
    - Encapsulated the process of creating and appending country details elements into a
      reusable createCountryDetailsElement function.
    - Utilized abstract functions within getCountryByName and displayCountryDetails for
      improved code readability and maintainability.

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const getCountryByName = async (countryName) => {
-    response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-    data = await response.json();
+const fetchCountryData = async (countryName) => {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+    const data = await response.json();
     return data;
-}
+};
 
-const switzerland = getCountryByName("Switzerland");
-
-const displayCountryDetails = async () => {
-
-    const countryData = await switzerland;
-
-    const countryDetails = {
-        name: countryData[0].name.common,
-        languages: Object.values(countryData[0].languages).join(`, `),
-        capital: countryData[0].capital,
-        population: countryData[0].population,
-    };
-
+const createCountryDetailsElement = (countryDetails) => {
     const countryContainer = document.querySelector('#country-stats');
 
     Object.entries(countryDetails).forEach((entry) => {
@@ -29,4 +17,22 @@ const displayCountryDetails = async () => {
     });
 };
 
-displayCountryDetails();
+const getCountryByName = async (countryName) => {
+    const countryData = await fetchCountryData(countryName);
+    return countryData;
+}
+
+const displayCountryDetails = async (countryName) => {
+
+    const countryData = await getCountryByName(countryName);
+    const countryDetails = {
+        name: countryData[0].name.common,
+        languages: Object.values(countryData[0].languages).join(`, `),
+        capital: countryData[0].capital,
+        population: countryData[0].population,
+    };
+
+    createCountryDetailsElement(countryDetails);
+};
+
+displayCountryDetails("Switzerland");
