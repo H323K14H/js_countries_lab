462fae5b7edde5c09081f7fb8f6fc6401fe52b76
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-28 17:11:59 +0000

    - Created displayCountryDetails() async function to display country details in a dynamic and structured manner - Ammended id name of section tag

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const getCountryByName = async (countryName) => {
     response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     data = await response.json();
-    console.log(data)
+    return data;
 }
 
-getCountryByName("Nigeria");
+const nigeria = getCountryByName("Nigeria");
+
+const displayCountryDetails = async () => {
+
+    const countryData = await nigeria;
+
+    const countryDetails = {
+        name: countryData[0].name.common,
+        languages: Object.values(countryData[0].languages),
+        capital: countryData[0].capital,
+        population: countryData[0].population,
+    };
+
+    const countryContainer = document.querySelector('#country-stats');
+
+    Object.entries(countryDetails).forEach((entry) => {
+        const [detailName, detailValue] = entry;
+
+        const detailElement = document.createElement('p');
+        detailElement.textContent = `${detailName}: ${detailValue}`;
+
+        countryContainer.appendChild(detailElement);
+    });
+};
+
+displayCountryDetails();
